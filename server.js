8d8b19b07b605953bed63ece3436f4843430b374
corentinmaillard <33122169+corentinmaillard@users.noreply.github.com>
2023-11-21 09:59:24 +0100

    start project

---
+++ server.js
@@ -0,0 +1,29 @@
+ // Set up express with socket.io
+ let express = require('express');
+ let mqtt = require('mqtt');
+ 
+ const app = express();
+ const server = require("http").Server(app);
+ const io = require("socket.io")(server);
+
+ // Replace these with your TTN MQTT connection details
+const ttnMqttHost = 'eu1.cloud.thethings.network';
+const ttnMqttPort = 1883; // Default MQTT port
+const ttnUsername = 'environment-watcher@ttn';
+const ttnPassword = 'NNSXS.E6FQVX2JN2N5UOGNYD6DFO3VMAEGCWOTJGDOIOY.7UHY5PJYEFGX2CRJAEW6G2WM55HDSUS5OVBWSNLOSE5MBS64XTIQ';
+const ttnDevice = 'eui-70b3d57ed0062cb4';
+
+ // Connect to TTN and subscribe to your device
+ const client = mqtt.connect(`mqtt://${ttnMqttHost}:${ttnMqttPort}`, {
+    username: ttnUsername,
+    password: ttnPassword,
+  });
+ client.on('connect', () => {
+    console.log('Connected to TTN MQTT');
+   client.subscribe(`${ttnUsername}/devices/${ttnDevice}/up`);
+ });
+
+ // When TTN sends a message, notify the client via socket.io
+ client.on('message', (topic, message) => {
+   io.emit('event-name', message);
+ });
