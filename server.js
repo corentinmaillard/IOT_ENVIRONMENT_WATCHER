74bf5f55044a61b0ef645b0de49398f95c79c887
radmou99 <mouradm.mm@outlook.com>
2023-12-06 16:21:50 +0100

    fixed stock problem

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Set up express with socket.io
 let express = require('express');
 let mqtt = require('mqtt');
-
+const fs = require('fs');
 const app = express();
 app.use(express.static('public'));
 const server = require("http").Server(app);
@@ -13,10 +13,32 @@ const ttnMqttPort = 1883; // Default MQTT port
 const ttnUsername = 'environment-watcher@ttn';
 const ttnPassword = 'NNSXS.5B7EE34DXWYT5TDNBTAQRO7YFTHDL7AJ6RN7LQI.XTWMASAFRVTKPSCUCWU7YWUPBVTSLX56QUFWP5SS67RSDXNYVIFQ';
 const ttnDevice = 'eui-70b3d57ed0062cb4';
-const temperature = [];
-const moisture = [];
-const soilmoisture = [];
-const light = [];
+let temperature = [];
+let moisture = [];
+let soilmoisture = [];
+let lightsensor = [];
+
+fs.readFile('./public/thing.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Erreur de lecture du fichier :', err);
+    return;
+  }
+
+  try {
+    const jsonData = JSON.parse(data);
+    // Maintenant, vous pouvez utiliser les données JSON (jsonData) comme vous le souhaitez.
+    console.log('Contenu du fichier JSON :', jsonData);
+    console.log('temp',jsonData.Temperature)
+    temperature = jsonData.Temperature
+    moisture = jsonData.Moisture
+    soilmoisture = jsonData.Soilmoisture
+    lightsensor = jsonData.Light
+  } catch (parseError) {
+    console.error('Erreur de parsing JSON :', parseError);
+  }
+});
+
+
 
 // Connect to TTN and subscribe to your device
 const client = mqtt.connect(`mqtt://${ttnMqttHost}:${ttnMqttPort}`, {
@@ -43,16 +65,25 @@ const client = mqtt.connect(`mqtt://${ttnMqttHost}:${ttnMqttPort}`, {
   const decodedPayload = jsonData.uplink_message.decoded_payload;
   const degreesC = decodedPayload.degreesC;
   const humidity = decodedPayload.humidity;
-  const soilHumidity= decodedPayload.soilHumidity
-  const mes = [degreesC, humidity,soilHumidity];
+  const soilHumidity= decodedPayload.soilHumidity;
+  const light = decodedPayload.lux;
+  const mes = [degreesC, humidity,soilHumidity,light];
 
-  console.log("Degrees Celsius:", mes[0]);
-  console.log("Humidity:", mes[1]);
-  console.log("soilHumidity:", mes[2]);
+  console.log("Temperature:", mes[0]);
+  console.log("Moisture:", mes[1]);
+  console.log("Soilmoisture:", mes[2]);
+  console.log("Light :",mes[3])
   io.emit('event-name', mes[0]);
-  temperature.push(mes[0]);
-  moisture.push(mes[1])
-  soilmoisture.push(mes[2])
+  // limit(temperature); 
+  add(temperature,mes[0])
+  add(moisture,mes[1])
+  add(soilmoisture,mes[2])
+  add(lightsensor,mes[3])
+  // temperature.push(mes[0]);
+  // moisture.push(mes[1])
+  // soilmoisture.push(mes[2])
+  // lightsensor.push(mes[3])
+  // console.log("shifted",temperature.shift())
   save()
 });
 
@@ -76,16 +107,24 @@ io.on("connection", function (socket) {
   io.emit('event-name', 'hello');
 })
 
-const fs = require('fs');
+// function limit(list){
+//   if (list.length() < 11){
+//     list.shift()
+//   }
+// }
 
-
+function add(list,thing){
+  list.push(thing)
+}
 
 function save(){
+  console.log(temperature)
+  
   const datas = {
     "Temperature": temperature,
     "Moisture": moisture,
-    "soilmoisture": soilmoisture,
-    "Light": light,
+    "Soilmoisture": soilmoisture,
+    "Light": lightsensor,
   };
   
   const dictString = JSON.stringify(datas, null, 2); // Adding indentation for better readability
