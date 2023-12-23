7a8171ff9f47a39e7035526c452ea07c1b4a2bb6
radmou99 <mouradm.mm@outlook.com>
2023-12-23 17:32:32 +0100

    shift down file

---
+++ Nodejs_BackEnd_FrontEnd/routes.js
@@ -0,0 +1,27 @@
+let express=require('express');
+let router=express.Router();
+
+router.get('/', (req,res)=>
+{
+    res.render("dashboard.ejs");
+});
+
+router.get('/moisture', (req,res)=>
+{
+    res.render("moisture.ejs");
+});
+
+router.get('/soilmoisture', (req,res)=>
+{
+    res.render("soilmoisture.ejs");
+});
+
+router.get('/light', (req, res) => {
+  res.render("light.ejs");
+});
+
+router.get('/temperature', (req, res) => {
+  res.render("temperature.ejs");
+});
+
+module.exports = router;
